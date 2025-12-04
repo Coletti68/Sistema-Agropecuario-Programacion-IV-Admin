e4bfe5acfbd8918fef32ebfee55ea8ffd75e91a8
mauriciozenon <maurizenon150603@gmail.com>
2025-12-04 00:51:29 -0300

    Reorganizacion de archivos .css

--- web-agropecuario-admin/sist_agropecuario/sist_agropecuario/settings.py
+++ web-agropecuario-admin/sist_agropecuario/sist_agropecuario/settings.py
@@ -73,13 +73,19 @@ TEMPLATES = [
 ]
 
 ASGI_APPLICATION = "sist_agropecuario.asgi.application"
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("127.0.0.1", 6379)],
+#         },
+#     },
+# }
+
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
-    },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
 
 
